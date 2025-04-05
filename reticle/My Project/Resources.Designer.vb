a5fb53b37be858a511906b3ee652067804df48a4
knappyflash <knappyflash2@gmail.com>
2025-04-04 22:18:28 -0700

    Did move

--- reticle/My Project/Resources.Designer.vb
+++ reticle/My Project/Resources.Designer.vb
@@ -93,16 +93,6 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property background() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("background", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
         Friend ReadOnly Property floppy_disc() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("floppy_disc", resourceCulture)
